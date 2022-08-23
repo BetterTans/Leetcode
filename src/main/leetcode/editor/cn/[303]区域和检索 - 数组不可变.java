4f73437a9a6585c7a35ab20387f1df0077c3cb8c
Tan <tangshaoquan920@gmail.com>
2022-08-24 00:25:25 +0800

    前缀和：[303]区域和检索 - 数组不可变

---
+++ src/main/leetcode/editor/cn/[303]区域和检索 - 数组不可变.java
@@ -0,0 +1,24 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class NumArray {
+    int[] preNums = null;
+
+    public NumArray(int[] nums) {
+        preNums = new int[nums.length + 1];
+        preNums[0] = 0;
+        for (int i = 1; i < preNums.length; i++) {
+            preNums[i] = preNums[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return preNums[right + 1] - preNums[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
