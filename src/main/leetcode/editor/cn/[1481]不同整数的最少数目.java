578c418f4befd904a10436a3d817bd481efe4807
Tan <tangshaoquan920@gmail.com>
2022-08-26 23:38:14 +0800

    [1481] 不同整数的最少数目

--- src/main/leetcode/editor/cn/[1481]不同整数的最少数目.java
+++ src/main/leetcode/editor/cn/[1481]不同整数的最少数目.java
@@ -2,21 +2,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> countMap = new HashMap<Integer, Integer>();
         for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            countMap.put(arr[i], countMap.getOrDefault(arr[i], 0) + 1);
         }
-        Map<Integer, Integer> m = map.entrySet().stream()
+        Map<Integer, Integer> map = countMap.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (old, b) -> old, LinkedHashMap::new));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (old, obj) -> old, LinkedHashMap::new));
         int index = 0;
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             k -= entry.getValue();
             if (k < 0) break;
-            i++;
+            index++;
         }
-        return m.size() - i;
+        return map.size() - index;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
