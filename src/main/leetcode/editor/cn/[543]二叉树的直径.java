b44a1729617dd8753d1ffc58d2b0b48aa4563f0d
Tan <tangshaoquan920@gmail.com>
2023-03-12 21:29:59 +0800

    543:二叉树的直径-递归

--- src/main/leetcode/editor/cn/[543]二叉树的直径.java
+++ src/main/leetcode/editor/cn/[543]二叉树的直径.java
@@ -28,35 +28,18 @@ public class TreeNode {
     }
 }
 class Solution {
-
-    private int depth = 0;
-    private int res = 0;
-
-    public int maxDepth(TreeNode root) {
-//        traverse(root);
-
-        return thrMaxDepth(root);
+    private int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxD(root);
+        return max;
     }
 
-    void traverse(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        depth++;
-        res = Math.max(depth, res);
-        traverse(root.left);
-        traverse(root.right);
-        depth--;
-    }
-
-    int thrMaxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        res = Math.max(leftDepth, rightDepth);
-        return res + 1;
+    private int maxD(TreeNode root) {
+        if (root == null) return 0;
+        int leftMax = maxD(root.left);
+        int rightMax = maxD(root.right);
+        max = Math.max(max, leftMax + rightMax);
+        return 1 + Math.max(leftMax, rightMax);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
