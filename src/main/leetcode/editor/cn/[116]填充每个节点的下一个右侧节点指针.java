214bbe3fbb617c842ce75eda4d734f35cacf2f31
Tan <tangshaoquan920@gmail.com>
2023-03-12 22:02:37 +0800

    116:填充每个节点的下一个右侧节点指针-队列/层序遍历

--- src/main/leetcode/editor/cn/[116]填充每个节点的下一个右侧节点指针.java
+++ src/main/leetcode/editor/cn/[116]填充每个节点的下一个右侧节点指针.java
@@ -2,30 +2,35 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/*
+
 // Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
+//class Node {
+//    public int val;
+//    public Node left;
+//    public Node right;
+//    public Node next;
+//
+//    public Node() {}
+//
+//    public Node(int _val) {
+//        val = _val;
+//    }
+//
+//    public Node(int _val, Node _left, Node _right, Node _next) {
+//        val = _val;
+//        left = _left;
+//        right = _right;
+//        next = _next;
+//    }
+//};
 class Solution {
     public Node connect(Node root) {
+        if (root == null) return null;
+        traverse(root.left, root.right);
+        return root;
+    }
+
+    private static Node getNode(Node root) {
         if (root == null) return null;
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
@@ -51,6 +56,16 @@ class Solution {
             }
         }
         return root;
+    }
+
+    public void traverse(Node left, Node right) {
+        if (left == null || right == null) {
+            return;
+        }
+        left.next = right;
+        traverse(left.left, left.right);
+        traverse(right.left, right.right);
+        traverse(left.right, right.left);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
