3f7f4c2c866e54f38af0337483d9e55c873adeac
Tan <tangshaoquan920@gmail.com>
2023-03-13 20:26:49 +0800

    236:二叉树的最近公共祖先-递归

--- src/main/leetcode/editor/cn/[236]二叉树的最近公共祖先.java
+++ src/main/leetcode/editor/cn/[236]二叉树的最近公共祖先.java
@@ -1,32 +1,25 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
-
 class Solution {
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return findLowestCommonAncestor(root, p, q);
-    }
-
-    public TreeNode findLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) return null;
         if (root.val == p.val || root.val == q.val) return root;
-        TreeNode left = findLowestCommonAncestor(root.left, p, q);
-        TreeNode right = findLowestCommonAncestor(root.right, p, q);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         if (left != null && right != null) {
             return root;
         }
-        return left != null ? left : right;
+        return left == null ? right : left;
     }
-
-
 }
 //leetcode submit region end(Prohibit modification and deletion)
