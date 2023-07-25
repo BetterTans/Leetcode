9d389652bf0b7b6271cfe1a69ea3521de615eaa9
Tan <tangshaoquan920@gmail.com>
2023-07-25 22:06:48 +0800

    114: 二叉树展开为链表-二叉树

--- src/main/leetcode/editor/cn/[114]二叉树展开为链表.java
+++ src/main/leetcode/editor/cn/[114]二叉树展开为链表.java
@@ -1,23 +1,5 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 
 class Solution {
     public void flatten(TreeNode root) {
